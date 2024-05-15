3ce257d53741752ac7d8416e9630a45497517109
iker-sebastian <iker.sebastian984@comunidadunir.net>
2024-05-15 16:32:01 +0200

    cambio error causa

--- RD_trafico_incidencias.py
+++ RD_trafico_incidencias.py
@@ -16,13 +16,14 @@ def API_incidencias(year, month, day):
         data_incidencias = data['incidences']
         for documento in data_incidencias:
             cityTown = documento.get('cityTown', 'road')
+            causa = documento.get('cause', 'Unkown cause')
             # Creo un diccionario con los datos que me interesan
             doc = {
                 '_id': documento['incidenceId'],
                 'sourceId': documento['sourceId'],
                 'incidenceType': documento['incidenceType'],
                 'province': documento['province'],
-                'cause': documento['cause'],
+                'cause': causa,
                 'cityTown': cityTown,
                 'startDate': documento['startDate'],
                 'road': documento['road']
